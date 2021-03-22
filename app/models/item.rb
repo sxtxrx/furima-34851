011ba94685692611199674f028988365f33d1a3e
sxxrx <sxtxrxfor.pg@gmail.com>
2021-03-22 19:37:52 +0900

    バリテージョン

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,19 @@ belongs_to :condition
 belongs_to :shippingfee
 belongs_to :prefecture
 belongs_to :shipDate
-
 belongs_to :user
 has_one_attached :image
 
-validates :
-
-
+with_options presence: true do
+validates :name
+validates :description
+validates :category
+validates :condition
+validates :shippingfee
+validates :prefecture
+validates :shipDate
+end
+validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:  9,999,999}
+validates :prefecture, numericality: {other_than: 0}
 
 end
