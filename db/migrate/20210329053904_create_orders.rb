8d45c02d0eea57371becbcc9429af8756e58d082
sxxrx <sxtxrxfor.pg@gmail.com>
2021-04-02 18:54:42 +0900

    fix

--- db/migrate/20210329053904_create_orders.rb
+++ db/migrate/20210329053904_create_orders.rb
@@ -5,7 +5,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.integer :prefecture_id,         null: false
       t.string :city,                   null: false
       t.string :address,                null: false
-      t.string :building_name,          null: false
+      t.string :building_name
       t.string :phone_number,           null: false
       t.bigint :history_id,             foreign_key: true
 
