4e780710b8cd115c2665206328cd8d3f567c7b27
sxxrx <sxtxrxfor.pg@gmail.com>
2021-04-05 15:23:22 +0900

    fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,7 +32,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = 'PAYJP_SECRET_KEY' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
       card: order_params[:token], # カードトークン
